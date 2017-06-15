4dd24574503a4f1b19c7ecc26f7cc268c8d9b89d
Monub <Monub@microsoft.com>
2017-06-15 14:14:41 -0700

    me

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Change the directory to %WINDIR%
+            //Environment.CurrentDirectory = 
+            //DirectoryInfo info = new DirectoryInfo(".");
+            //Console.WriteLine("Directory Info:   " + info.FullName);
+            Console.WriteLine("Hello:   " + Environment.GetEnvironmentVariable("Name"));
+
+        }
+    }
+}
